13729ad993543bae5a762e69636fdea8821bab63
Bradley Haveman <bradley@haveman.ca>
2023-02-05 23:01:33 -0500

    More tables

--- src/utils/lookup.ts
+++ src/utils/lookup.ts
@@ -1,7 +1,8 @@
 // Importing data files
 import raw from '../data/RawMaterials.json';
 import products from '../data/Products.json';
-import curiosities from '../data/Curiosity.json';
+import cooking from '../data/Cooking.json';
+import curiosities from '../data/Curiosities.json';
 import conTech from '../data/ConstructedTechnology.json';
 
 // Defining the interface for an Item
@@ -11,48 +12,50 @@ type Item = {
 	Description: string;
 	Icon: string;
 	Colour: string;
-  BaseValueUnits: number;
+	BaseValueUnits: number;
 };
 
 // Mapping the prefixes of item id to the actual data sources
 const dataSources = {
-  raw,
-  prod: products,
-  cur: curiosities,
-  conTech
+	raw,
+	prod: products,
+	cook: cooking,
+	cur: curiosities,
+	conTech,
 };
 
 // Mapping the prefixes of item id to the corresponding slugs
 const slugs = {
-  raw: '/raw/',
-  prod: '/products/',
-  cur: '/curiosities/'
+	raw: '/raw/',
+	prod: '/products/',
+	cook: '/cooking/',
+	cur: '/curiosities/',
 };
 
 // Returns the slug corresponding to the item id
-const getSlug = (id: string ): string => {
-  // Extract the prefix of the item id by splitting the id by its numeric part
-  const prefix = id.split(/\d/)[0];
-  // Return the slug corresponding to the prefix or "item" if prefix not found
-  return slugs[prefix] || 'item';
+const getSlug = (id: string): string => {
+	// Extract the prefix of the item id by splitting the id by its numeric part
+	const prefix = id.split(/\d/)[0];
+	// Return the slug corresponding to the prefix or "item" if prefix not found
+	return slugs[prefix] || 'item';
 };
 
 // Returns the item corresponding to the id from a given data source
 const findById = (source: Item[], id: string): Item => {
-  // Find the item from the data source with a matching id
-  return source.find(p => p.Id === id);
+	// Find the item from the data source with a matching id
+	return source.find((p) => p.Id === id);
 };
 
 // Returns the item corresponding to the id from the appropriate data source
 const getById = (id: string): Item => {
-  // Extract the prefix of the item id by splitting the id by its numeric part
-  const prefix = id.split(/\d/)[0];
-  // Get the data source corresponding to the prefix
-  const source = dataSources[prefix];
-  // If source not found, return undefined
-  if (!source) return;
-  // Find the item from the source and return it
-  return findById(source, id);
+	// Extract the prefix of the item id by splitting the id by its numeric part
+	const prefix = id.split(/\d/)[0];
+	// Get the data source corresponding to the prefix
+	const source = dataSources[prefix];
+	// If source not found, return undefined
+	if (!source) return;
+	// Find the item from the source and return it
+	return findById(source, id);
 };
 
 // Export the getSlug and getById functions, and the Item interface
