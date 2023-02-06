5be976b44bafb4bc4ed7caded5fa8395628ddb3f
Bradley Haveman <bradley@haveman.ca>
2023-02-05 20:10:01 -0500

    SEO Things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [image()],
+  site: "https://nms.vercel.app/",
+  integrations: [image(), sitemap()]
 });
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+
+export const SITE = {
+    website: "https://nms.vercel.app/",
+    author: "Bradley Haveman",
+    desc: "Looking for one stop shop for all your No Man's Sky needs? Look no more! No Man's Sky Refiner Recipes, Crafting Guide and Cooking Guide",
+    title: "No Man's Sky Recipes",
+    ogImage: "astropaper-og.jpg",
+  };
