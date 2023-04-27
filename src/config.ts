5cc032221c6a22bfd3cb6d260d3272f820df76d1
Bradley Haveman <bradley@haveman.ca>
2023-04-27 12:25:09 -0400

    Updated to 4.2

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE = {
     author: "Bradley Haveman",
     desc: "Unlock new possibilities in No Man's Sky with our comprehensive list of in game items and recipes. From rare resources to exotic ingredients, our guide has everything you need to take your game to the next level. Discover the secrets of the universe today!",
     title: "No Man's Sky Recipes",
-    version: 4.1,
-    version_name: "Fractal",
-    version_link: "https://www.nomanssky.com/fractal-update/",
+    version: 4.2,
+    version_name: "Interceptor",
+    version_link: "https://www.nomanssky.com/interceptor-update/",
 };
