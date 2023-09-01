229e0052dfcd4d5e3a7d917484b646f8caaaf9d4
Bradley Haveman <bradley@haveman.ca>
2023-09-01 11:43:55 -0400

    Upgrading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
@@ -8,9 +7,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nms.vercel.app/",
-  integrations: [image(), sitemap(), tailwind({
+  integrations: [ react(),sitemap(), tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), react()]
+  })]
 });
