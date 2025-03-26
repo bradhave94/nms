706c07b2992ac8c6c790565982eecb8b2a5ebcdb
Bradley Haveman <bradley@haveman.ca>
2025-03-26 09:06:30 -0400

    updated to worlds part 2

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ export const SITE = {
     author: "Bradley Haveman",
     desc: "Unlock new possibilities in No Man's Sky with our comprehensive list of in game items and recipes. From rare resources to exotic ingredients, our guide has everything you need to take your game to the next level. Discover the secrets of the universe today!",
     title: "No Man's Sky Recipes",
-    version: 5.21,
-    version_name: "Aquarius",
-    version_link: "https://www.nomanssky.com/2024/09/aquarius-update/",
+    version: 5.5,
+    version_name: "Worlds Part II",
+    version_link: "https://www.nomanssky.com/worlds-part-ii-update/",
     adsense_id: 'ca-pub-8524094599848175',
 };
