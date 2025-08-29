e373d407a304782d11a763f5527b322668308263
Bradley Haveman <bradley@haveman.ca>
2025-08-29 08:08:49 -0400

    Updated to voyagers

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ export const SITE = {
     author: "Bradley Haveman",
     desc: "Unlock new possibilities in No Man's Sky with our comprehensive list of in game items and recipes. From rare resources to exotic ingredients, our guide has everything you need to take your game to the next level. Discover the secrets of the universe today!",
     title: "No Man's Sky Recipes",
-    version: 5.7,
-    version_name: "Beacon",
-    version_link: "https://www.nomanssky.com/beacon-update/",
+    version: 6.0,
+    version_name: "Boyagers",
+    version_link: "https://www.nomanssky.com/voyagers-update",
     adsense_id: 'ca-pub-8524094599848175',
 };
